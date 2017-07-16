603b966f858fab664fea89bdb2601104a2d5c791
Andres Ibarra Meade <aibarra@umich.edu>
2017-07-16 15:15:00 -0400

    Fixed the HTML layout format

--- src/App/gui/ItemRender.java
+++ src/App/gui/ItemRender.java
@@ -15,24 +15,39 @@ public class ItemRender extends JLabel implements ListCellRenderer<Book> {
     public Component getListCellRendererComponent(JList<? extends Book> list, Book book, int index, boolean isSelected, boolean cellHasFocus) {
 
         String rowHTML = "";
-        if (!(book.getStatus()== Status.CHECKED_OUT)) {
+        String braille = "no";
+        if (book.getBraille()) {
+            braille = "Yes";
+            rowHTML = getString(book, rowHTML, braille);
+
+        }
+        else {
+            braille = "No";
+            rowHTML = getString(book, rowHTML, braille);
+        }
+        setText(rowHTML);
+        return this;
+    }
+
+    private String getString(Book book, String rowHTML, String braille) {
+        if (!(book.getStatus() == Status.CHECKED_OUT)) {
             rowHTML = "<html><ul>\n" +
                     "<li>\n" + "<h3>" + book.getTitle() + "</h3>\n" +
                     "<p><strong>Author: </strong>" + book.getAuthor() +
                     "<br><strong>Status: </strong>" + book.getStatus() +
                     "<br><strong>Genre: </strong>" + book.getGenre() +
+                    "<br><strong>Braille: </strong>" + braille +
                     "</li></ul>\n";
-        }
-       else if (book.getStatus()== Status.CHECKED_OUT) {
+        } else if (book.getStatus() == Status.CHECKED_OUT) {
             rowHTML = "<html><ul>\n" +
                     "<li>\n" + "<h3>" + book.getTitle() + "</h3>\n" +
-                   "<p><strong>Author: </strong>" + book.getAuthor() +
-                   "<br><strong>Status: </strong>" + book.getStatus() +
-                   "<br><strong>Genre: </strong>" + book.getGenre() +
-                   "<br><strong>Due Date: </strong>" + book.getDueDate() +
-                   "</li></ul>\n";
-       }
-        setText(rowHTML);
-        return this;
+                    "<p><strong>Author: </strong>" + book.getAuthor() +
+                    "<br><strong>Status: </strong>" + book.getStatus() +
+                    "<br><strong>Genre: </strong>" + book.getGenre() +
+                    "<br><strong>Braille: </strong>" + braille +
+                    "<br><strong>Due Date: </strong>" + book.getDueDate() +
+                    "</li></ul>\n";
+        }
+        return rowHTML;
     }
 }
